3e0c29a7a4e05cd5bddaeaac4549cbe3c7249dcc
vikenty19 <vikenty.plakhov@testpro.io>
2024-12-23 17:27:15 -0500

    cucmber playsong DDtest added

---
+++ src/test/java/StepDefinitions/PlaySongStep.java
@@ -0,0 +1,60 @@
+package StepDefinitions;
+
+import io.cucumber.java.After;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+
+public class PlaySongStep {
+
+    private final LoginStepDefinitions loginStepDefinitions;
+    public PlaySongStep(){
+        this.loginStepDefinitions = new LoginStepDefinitions();
+
+    }
+
+    @Given("I am logged in ahd on SongPage {string},{string}")
+    public void iAmLoggedIn(String email,String password){
+        loginStepDefinitions.setUpDriver();
+        loginStepDefinitions.iOpenLoginPage();
+        loginStepDefinitions.iEnterEmail(email);
+        loginStepDefinitions.iEnterPassword(password);
+        loginStepDefinitions.iClickSubmit();
+        loginStepDefinitions.iLoggedIn();
+
+    }
+
+
+    @And("Click on the song title {string}")
+    public void clickOnTheSongName(String songName) {
+           loginStepDefinitions.driver.findElement(By.xpath("//td[contains(text(), '"+songName+"')]")).click();
+
+    }
+
+    @And("click on the play button")
+    public void clickOnThePlayButton() {
+
+      WebElement playBtn =  loginStepDefinitions.driver.findElement(By.cssSelector(".play .fa-play"));
+      new Actions(loginStepDefinitions.driver)
+              .moveToElement(playBtn)
+              .perform();
+      playBtn.click();
+
+    }
+
+
+
+    @Then("Equalizer button is appeared")
+    public void equalizerButtonIsAppeared() {
+        Assert.assertTrue(loginStepDefinitions.driver.findElement(By.cssSelector("div.bars")).isDisplayed());
+    }
+    @After
+    public void tearDown(){
+        loginStepDefinitions.driver.quit();
+    }
+}
--- src/test/resources/features/cucu.feature
+++ src/test/resources/features/cucu.feature
@@ -1,17 +1,14 @@
 Feature: Play Song
-  Scenario:
-    Given I open browser
-    When I open login page
 
-When i enter valid email "galy.o@testpro.io"
-And I enter valid password "te$t$tudent1"
-And  I click Submit
-And I am logged in
-#Examples:
-#|email                  |   password|
-#|"galy.o@testpro.io"|"te$t$tudent1"|
+  Scenario Outline:
+
+   Given I am logged in ahd on SongPage <email>,<password>
     And User click on AllSongs button
-    And Click on the song title <SongName>
+    And Click on the song title <songName>
     And click on the play button
-    Then Song is reproduced
-    And Equalizer button is appeared
+
+    Then Equalizer button is appeared
+    Examples:
+      | email               | password       | songName|
+      | "galy.o@testpro.io" | "te$t$tudent1" | "Episode 2"|
+      | "galy.o@testpro.io" | "te$t$tudent1" | "Pluto"|
